21a81fbef4f38f33fb4e7668590517929cc71db9
seunghyun <koreas9408@gmail.com>
2025-03-09 16:17:22 +0900

    feat: [미션1] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/main/kotlin/co/kr/nadosolo/NadosoloApplication.kt
@@ -0,0 +1,11 @@
+package co.kr.nadosolo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NadosoloApplication
+
+fun main(args: Array<String>) {
+	runApplication<NadosoloApplication>(*args)
+}
---
+++ src/test/kotlin/co/kr/nadosolo/nadosolo/NadosoloApplicationTests.kt
@@ -0,0 +1,13 @@
+package co.kr.nadosolo.nadosolo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NadosoloApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
